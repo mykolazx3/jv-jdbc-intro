84db8c1108a22a62221f4162ba65d88d8e02fe98
Mykola <113469067+mykolazx3@users.noreply.github.com>
2024-03-25 21:42:35 +0200

    Update ConnectionUtil.java

--- src/main/java/mate/academy/util/ConnectionUtil.java
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -10,7 +10,7 @@ public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
     private static final Properties DB_PROPERTIES;
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "777441";
+    private static final String PASSWORD = "1234";
 
     static {
         DB_PROPERTIES = new Properties();
